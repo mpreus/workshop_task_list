f4a7fb521aa7e27643317e728f617f4e03cfcb0d
Maciej Preus <mpreus@onet.eu>
2019-09-09 12:51:03 +0200

    Add some improvement to the JS functionalities

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 JavaScript used to help remembering important tasks
 Internet site with an input element to write down you tasks for the day (week / month).
 The site will create a list with options to mark each particular task as done and to remove it from the list.
+
+dodać 
+1) w JS: mouseover i mouseleave na przyciski, by dodać efekty 'hover'
+1a) w JS: SweetAlert2 zamiast przeglądarkowego alertu
+2) w CSS: font-family z GoogleFonts
+
+3) w CSS: obrazek z Undraw / ramka wkoło całości?!
--- js/app.js
+++ js/app.js
@@ -1,53 +1,54 @@
 document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-	var inputElement = document.getElementById("taskInput"); 
-	var addTaskButton = document.getElementById("addTaskButton"); 
-	var list = document.getElementById("taskList"); 		
-	var removeTasks = document.getElementById("removeFinishedTasksButton"); 
-	var counterSpan = document.getElementById("counter"); 
+	let inputElement = document.getElementById("taskInput"); 
+	let addTaskButton = document.getElementById("addTaskButton"); 
+	let list = document.getElementById("taskList"); 		
+	let removeTasks = document.getElementById("removeFinishedTasksButton"); 
+	let counterSpan = document.getElementById("counter"); 
 	
-	counterSpan.innerText = 0; 			//initial value
+	counterSpan.innerText = 0; 							// initial value of the tasks number
 	
 // function to add new task:
 	addTaskButton.addEventListener("click", takeText);
 
 	function takeText(event) { 
 		event.preventDefault(); 							// to prevent re-loading
-		var taskContent = inputElement.value; 				// task text
+		let taskContent = inputElement.value; 				
 		if (taskContent.length < 5 || taskContent > 99) {
-			alert("Your task should contain 6 to 99 characters");
-// to prevent further writing when the task text is too short:
+			alert("Your task should contain 5 to 99 characters");
+// to prevent further writing when the task text seems to be too short:
 			inputElement.value = "";
 			takeText();
 		}
 
-		var newTask = document.createElement("li"); 
+		let newTask = document.createElement("li"); 
 
 		list.appendChild(newTask).classList.add("taskItem"); 
 
-		var newTaskLine = document.createElement("p");
+		let newTaskLine = document.createElement("p");
 		newTask.appendChild(newTaskLine); 
 		newTaskLine.innerText = inputElement.value;
 
 // add two buttons for each task
 // DELETE the task 
-		var newButtonDelete = document.createElement("button");
+		let newButtonDelete = document.createElement("button");
 		newTask.appendChild(newButtonDelete).classList.add("removeTaskButton");
 		newButtonDelete.innerText = "Delete";
 		newButtonDelete.addEventListener('click', remove);
 		function remove(event) {
 			newTask.parentElement.removeChild(newTask);
+			let taskNumber = document.querySelectorAll("li.taskItem");  
+			counterSpan.innerText = taskNumber.length;
 		}
 
 // COMPLETE the task
-		var newButtonComplete = document.createElement("button");
+		let newButtonComplete = document.createElement("button");
 		newTask.appendChild(newButtonComplete).classList.add("setCompleteButton");
 		newButtonComplete.innerText = "Complete";
-		
 		inputElement.value = ""; 
 
-// completed task in red
+	// completed task in red
 		newButtonComplete.addEventListener("click", makeTaskContentRed);
 		function makeTaskContentRed(event) {
 			event.preventDefault();
@@ -61,27 +62,22 @@ function init() {
 			}
 
 		/* task counter on the site */
-			var taskNumber = document.querySelectorAll("li.taskItem"); 
-			console.log(taskNumber.length); 
+			let taskNumber = document.querySelectorAll("li.taskItem");  
 			counterSpan.innerText = taskNumber.length;
 		} 
 
-		var taskNumber = document.querySelectorAll("li.taskItem"); 
-			console.log(taskNumber.length); 
-			counterSpan.innerText = taskNumber.length;
+		let taskNumber = document.querySelectorAll("li.taskItem");  
+		counterSpan.innerText = taskNumber.length;
 	} 
 	
-// aby usuwać zadania wykonane (z czerwonym tekstem), ustawiamy na guzik 'Remove finished tasks' 
-// nasłuch i uruchamiamy funkcję
+// 'Remove finished tasks' function for removing all tasks marked as completed
 		removeTasks.addEventListener("click", removeAllDone);
 		function removeAllDone(event) {
-			var toDelete = document.querySelectorAll("li.done"); // do usunięcia elementy 'li'
-			for (var i = 0; i < toDelete.length; i++) {
+			let toDelete = document.querySelectorAll("li.done"); 
+			for (let i = 0; i < toDelete.length; i++) {
 				toDelete[i].parentElement.removeChild(toDelete[i]);
 			}
 		}
-/* tu tez można zmniejszać licznik liczby zadań do wykonania */
-
 }
 
 
